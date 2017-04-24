b0b27dcfc6ede40662a64d269b0526cbede2c4bf
Alex Roeum <avroeum@gmail.com>
2017-04-23 21:29:00 -0400

    Trying histogram with please.csv

--- projhistogram.js
+++ projhistogram.js
@@ -34,18 +34,18 @@ var yAxis = d3.svg.axis()
 var bardata = [ ];
 
 //this variable sets the current state of the graph to ascending, and will help fluctuate the graph's movement
-var currstate= 1;	
+var currstate= 1;
 
 //The data from loyalty_data.svg is read and the chart is made
-d3.tsv("loyalty_data.tsv", type, function(error, data) {
+d3.csv("please.csv", type, function(error, data) {
  bardata= data;
  checkbardata= data;
- 
+
     //X and Y's dimensions are set
     x.domain(data.map(function(d) { return d.FirstName; }));
     y.domain([0, d3.max(data, function(d) { return d.price; })]);
 
-    //The x-axis is made and put in the bottom 
+    //The x-axis is made and put in the bottom
     chart.append("g")
 	   .attr("class", "axis")
 	   .attr("transform", "translate(0," + height + ")")
@@ -65,8 +65,8 @@ d3.tsv("loyalty_data.tsv", type, function(error, data) {
 	   .attr("y", function(d) { return y(d.price); })//this is the y-position
 	   .attr("height", function(d) { return height - y(d.price); })//Sets the sixe of the rectangle
 	   .attr("width", x.rangeBand()); //The width is set
-	 
-	  
+
+
 });
 
 
@@ -76,13 +76,13 @@ d3.select("svg")
 	//the variables of where the user clicks
 	var x=d3.mouse(this)[0]
 	var y =d3.mouse(this)[1]
-	
+
 	//tests to see if the user clicks the x axis or below
-	
+
 	if(x>=40 || y<=0){
 	//console.log("x"+ d3.mouse(this)[0] +"y"+d3.mouse(this)[1]);
-	
-	//checks to see if the current state is ascending 
+
+	//checks to see if the current state is ascending
 	if (currstate == 1){
 	//change the bar data to descending in regards to the FirstName data
 	bardata.sort(function(a,b) {return d3.descending(a.FirstName, b.FirstName)});
@@ -90,11 +90,11 @@ d3.select("svg")
 	redrawXDescending();
 	//set the current state to 0
 	currstate=0;
-	}	
-	
-	//checks to see if the current state is 0 (descending) 
+	}
+
+	//checks to see if the current state is 0 (descending)
 	else if(currstate == 0){
-		
+
 		//set the bardata to ascending in regards to the FirstName data
 		bardata.sort(function(a,b) {return d3.ascending(a.FirstName, b.FirstName)});
 		//redraw the graph ascending in regards to X
@@ -103,14 +103,14 @@ d3.select("svg")
 		currstate=1;
 	}
 
-	
-		
-		
+
+
+
 	}
-	
+
 	//test to see if the user clicked the y axis or over
 	else if (x<=40 || y>=0){
-		
+
 		//tests to see if the currstate is 1
 		if(currstate == 1){
 		//sets the bar data into descending order in regards to price
@@ -118,7 +118,7 @@ d3.select("svg")
 		//redraws the graph descending in regards to y
 		redrawYDescending();
 		//sets the current state to 0 so the program ascends the data next time
-		currstate=0;	
+		currstate=0;
 		}
 		//tests to see if the current state is 0
 		else if(currstate==0){
@@ -127,14 +127,14 @@ d3.select("svg")
 		//redraws the graph ascending in regards to Y
 		redrawYAscending();
 		//sets the current state to 1
-		currstate=1;	
+		currstate=1;
 		}
-		
-	
-	
+
+
+
 	}
 }
-	
+
 );
 
 
@@ -145,22 +145,22 @@ d3.select("svg")
 
 //this function redraws the graph in descending order in regards to X
 function redrawXDescending(){
-	
+
 	//sets the domain for x and y
 	 x.domain(bardata.map(function(d) { return d.FirstName; }));
     y.domain([0, d3.max(bardata, function(d) { return d.price; })]);
 
-	
+
  //the x-axis is made and put on the bottom
- //***this is where I had trouble. I couldnt find the function that erases the current X-axis labels, 
+ //***this is where I had trouble. I couldnt find the function that erases the current X-axis labels,
  // Thats why my graph has x-labels made on top of each other
- // I looked on the internet for the functions and couldnt find the right one. 
+ // I looked on the internet for the functions and couldnt find the right one.
  //I kept accidently erasing the whole X-axis, so instead I just left the x-labels overlapping each other :( *****
     chart.append("g")
 	   .attr("class", "axis")
 	   .attr("transform", "translate(0," + height + ")")
 	   .call(xAxis);
-	
+
 
     //the y-axis is made
     chart.append("g")
@@ -184,7 +184,7 @@ function redrawXAscending(){
 	x.domain(bardata.map(function(d) { return d.FirstName; }));
     y.domain([0, d3.max(bardata, function(d) { return d.price; })]);
 
-	 //the x-axis is made and put at the bottom 
+	 //the x-axis is made and put at the bottom
     chart.append("g")
 	   .attr("class", "axis")
 	   .attr("transform", "translate(0," + height + ")")
@@ -204,8 +204,8 @@ function redrawXAscending(){
 		  .attr("x", function (d) {return x(d.FirstName); })//this is the x-position
 		  .attr("height", function(d) { return height - y(d.price); })//Sets the size of the rectangle
 	   .attr("width", x.rangeBand());// the width is set
-	
-	
+
+
 }
 
 //this function redraws the graph in descending order in regards to Y
@@ -259,9 +259,9 @@ chart.selectAll("rect")
 		  .attr("x", function (d) {return x(d.FirstName); })//this is the x-position
 		  .attr("height", function(d) { return height - y(d.price); })//sets the size of the rectangle
 	   .attr("width", x.rangeBand());//sets the width
-	
-	
-	
+
+
+
 }
 
 
